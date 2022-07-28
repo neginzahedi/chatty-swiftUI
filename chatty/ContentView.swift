560f81f59df35fdc8efc59d1754a0353d2a7dffa
Negin Zahedi <zahedi.negin@gmail.com>
2022-07-28 19:38:42 -0400

    Initial Commit

---
+++ chatty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chatty/chattyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chattyApp.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-07-28.
+//
+
+import SwiftUI
+
+@main
+struct chattyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
