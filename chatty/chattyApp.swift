d01cb2c553577a72733461e6863cc636b8166944
Negin Zahedi <zahedi.negin@gmail.com>
2022-07-31 17:28:47 -0400

    ContentView changed to HomeView

--- chatty/chattyApp.swift
+++ chatty/chattyApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct chattyApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
