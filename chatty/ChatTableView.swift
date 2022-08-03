deba89a4f2797e0fad83c12eaa04928623a47998
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-03 12:13:05 -0400

    new message and setting added

--- chatty/ChatTableView.swift
+++ chatty/ChatTableView.swift
@@ -8,86 +8,120 @@
 import SwiftUI
 
 struct ChatTableView: View {
+    
+    @State var showSettingOptions = false
+    
     var body: some View {
         NavigationView{
             VStack{
-                // user navbar
-                HStack(spacing: 16){
-                    // user image
-                    Image(systemName: "person.fill")
-                        .font(.system(size: 34, weight: .heavy))
-                    
-                    VStack(alignment: .leading, spacing: 4){
-                        // user username
-                        Text("username")
-                            .font(.system(size: 24,weight: .bold))
-                        
-                        HStack{
-                            Circle()
-                                .foregroundColor(.green)
-                                .frame(width: 14, height: 14)
-                            // user status
-                            Text("online")
-                                .font(.system(size: 12))
-                                .foregroundColor(.gray)
-                        }
-                    }
-                    Spacer()
-                    Image(systemName: "gear")
-                }.padding()
-                
-                // user messages
-                ScrollView{
-                    ForEach(0 ..< 10, id:\.self){ num in
-                        VStack{
-                            HStack(spacing: 16) {
-                                Image(systemName: "person.fill")
-                                    .font(.system(size: 32))
-                                    .padding(8)
-                                    .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black,lineWidth: 1))
-                                
-                                VStack(alignment: .leading){
-                                    Text("username")
-                                        .font(.system(size: 14,weight: .bold))
-                                    Text("messages")
-                                        .font(.system(size: 14))
-                                        .foregroundColor(.gray)
-                                }
-                                
-                                Spacer()
-                                
-                                Text("2m")
-                                    .font(.system(size: 14,weight: .semibold))
-                            }
-                            Divider()
-                                .padding(.vertical,8)
-                        }.padding(.horizontal)
-                    }
-                    
-                }
-                
+                // Nav bar: User Profile
+                userProfile
+                // ScrollView: Sender Message List
+                sendersList
             }
-            .overlay(
-                Button{
-                    
-                }label: {
-                    HStack{
-                        Spacer()
-                        Text("+ New Message")
-                            .font(.system(size: 16, weight: .bold))
-                        Spacer()
-                    }
-                    .foregroundColor(.white)
-                    .padding(.vertical)
-                    .background(Color.blue)
-                    .cornerRadius(35)
-                    .padding(.horizontal)
-                    shadow(radius: 15)
-                },alignment: .bottom)
-            
-            // hide nav bar
+            .overlay(newMessageButton,alignment: .bottom)
             .navigationBarHidden(true)
+        }
+    }
+    
+    // HStack: User Profile
+    private var userProfile: some View{
+        HStack(spacing: 16){
+            // user image
+            Image(systemName: "person.fill")
+                .font(.system(size: 34, weight: .heavy))
+                .padding(8)
+                .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black,lineWidth: 1))
             
+            VStack(alignment: .leading, spacing: 4){
+                // user username
+                Text("username")
+                    .font(.system(size: 24,weight: .bold))
+                
+                HStack{
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 14, height: 14)
+                    // user status
+                    Text("online")
+                        .font(.system(size: 12))
+                        .foregroundColor(Color(.lightGray))
+                }
+            }
+            
+            Spacer()
+            Button{
+                showSettingOptions.toggle()
+            }label: {
+                Image(systemName: "gear")
+                    .font(.system(size: 24,weight: .bold))
+                    .foregroundColor(Color(.label))
+            }
+        }.padding()
+            .actionSheet(isPresented: $showSettingOptions) {
+                .init(title: Text("Settings"),buttons: [.destructive(Text("Sign Out"),action: {
+                    // TODO: Signout
+                    
+                }),
+                                                        .cancel()
+                                                       ])
+            }
+    }
+    
+    // ScrollView: Senders List
+    private var sendersList: some View{
+        ScrollView{
+            ForEach(0 ..< 10, id:\.self){ num in
+                VStack{
+                    // Sender
+                    senderMessageRow
+                    Divider()
+                        .padding(.vertical,8)
+                }.padding(.horizontal)
+            }.padding(.bottom,50)
+        }
+    }
+    
+    // HStack: Sender
+    private var senderMessageRow: some View{
+        HStack(spacing: 16) {
+            Image(systemName: "person.fill")
+                .font(.system(size: 32))
+                .padding(8)
+                .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black,lineWidth: 1))
+            
+            VStack(alignment: .leading){
+                Text("Username")
+                    .font(.system(size: 16,weight: .bold))
+                Text("Text messages")
+                    .font(.system(size: 14))
+                    .foregroundColor(Color(.lightGray))
+            }
+            
+            Spacer()
+            
+            Text("2m")
+                .font(.system(size: 14,weight: .semibold))
+        }
+    }
+    
+    // Button: Create New Message
+    private var newMessageButton: some View{
+        Button{
+            
+        }label: {
+            HStack{
+                Spacer()
+                Text("+ New Message")
+                    .font(.system(size: 16, weight: .bold))
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding(.vertical)
+            .background(Color.blue)
+            .cornerRadius(35)
+            .padding(.horizontal)
+            .shadow(radius: 15)
         }
     }
 }
