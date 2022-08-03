21632d67238e391f842ff75ee318842984029171
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-03 12:11:57 -0400

    display alerts on screen

--- chatty/SignUpView.swift
+++ chatty/SignUpView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Negin Zahedi on 2022-07-28.
 //
+// SignUpView has: 1.Image 2.Text 3.TextField 4.Buttons
 
 import SwiftUI
 import Firebase
@@ -15,63 +16,81 @@ struct SignUpView: View {
     @State private var password: String = ""
     @State private var confirmPassword: String = ""
     
-    @State private var showingAlert = false
-
+    @State private var alertMessage: String = ""
+    
+    @State private var confirmPassAlert = false
+    @State private var accountFaildAlert = false
+    @State private var accountcreatedAlert = false
+    
     
     var body: some View {
+        
+        // Vertical Contents
         VStack(alignment: .center){
             Image("signup-view-img")
                 .resizable()
                 .scaledToFit()
                 .frame(height: 80)
             
-            // Sign-up VStack
+            // VStack - Sign-up Field
             VStack(alignment: .leading){
+                // Text
                 Text("Sign-up")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                 
-                // username
+                // VStack - username
                 VStack(alignment: .leading){
+                    // Text
                     Text("Username")
                         .font(.callout)
                         .bold()
+                    // TextField
                     TextField("Enter username...", text: $username)
                         .disableAutocorrection(true)
                         .textFieldStyle(.roundedBorder)
                 }.padding(5)
-                // email address OR phone number
+                
+                // VStack - email address
                 VStack(alignment: .leading){
+                    // Text
                     Text("Email")
                         .font(.callout)
                         .bold()
+                    // TextField
                     TextField("Enter email address...", text: $email)
                         .disableAutocorrection(true)
                         .textFieldStyle(.roundedBorder)
                 }.padding(5)
-                // password and confirmation
+                
+                // VStack - password and confirmation
                 VStack(alignment: .leading){
+                    // Text
                     Text("Password")
                         .font(.callout)
                         .bold()
+                    // SecureField
                     SecureField("Enter password...", text: $password)
                         .disableAutocorrection(true)
                         .textFieldStyle(.roundedBorder)
                     
                 }.padding(5)
-                // confirm password
+                
+                // VStack - confirm password
                 VStack(alignment: .leading){
+                    // Text
                     Text("Confirm Password")
                         .font(.callout)
                         .bold()
+                    // SecureField
                     SecureField("Enter password again...", text: $confirmPassword)
                         .disableAutocorrection(true)
                         .textFieldStyle(.roundedBorder)
                 }.padding(5)
             }.padding()
+            // end of sign-up field
             
-            
-            // Button: create account
+            // Button: Sign-up
             Button("Sign Up", action: createAccount)
                 .frame(width: 200, height: 50, alignment: .center)
                 .background(.blue)
@@ -79,9 +98,12 @@ struct SignUpView: View {
                 .foregroundColor(.white)
                 .font(.headline)
                 .padding()
-            // sign in
+            
+            // HStack - sign in
             HStack(alignment:.center){
+                // Text
                 Text("I already have an account.")
+                // NavigationLink
                 NavigationLink(destination: SignInView()){
                     Text("Sign-in")
                         .foregroundColor(.gray)
@@ -89,28 +111,46 @@ struct SignUpView: View {
             }
             
         }.padding()
+        // end of content field
         
-            .alert("Password and confirm password are not same.", isPresented: $showingAlert) {
+            .alert("The password confirmation does not match.", isPresented: $confirmPassAlert) {
                 Button("Cancel", role: .cancel) { }
             }
         
+            .alert(isPresented: self.$accountFaildAlert,
+                   content: { self.showAlert() })
+        
+            .alert(isPresented: self.$accountFaildAlert,
+                   content: { self.showAlert() })
+        
+            .alert(isPresented: self.$accountcreatedAlert,
+                   content: { self.showAlert() })
     }
     
     private func createAccount(){
         if password == confirmPassword{
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error{
-                    print("Failed to create user: \(e)")
+                    self.alertMessage = "\(e.localizedDescription)"
+                    accountFaildAlert = true
+                    
                 }else{
-                    print("new user account created.\(authResult?.user.uid ?? "")")
+                    self.alertMessage = "Account created!"
+                    accountcreatedAlert = true
                 }
             }
         } else{
-            showingAlert = true
-            print("passwords are not same.")
+            self.alertMessage = "The password confirmation does not match."
+            self.confirmPassAlert = true
         }
     }
     
+    func showAlert()-> Alert{
+        Alert(
+            title: Text("Error"),
+            message: Text(self.alertMessage),
+            dismissButton: .default(Text("Okay")))
+    }
 }
 
 struct SignUpView_Previews: PreviewProvider {
