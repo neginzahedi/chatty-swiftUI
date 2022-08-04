cb7d20a4f948aa68e398450605aaa1c32d54684b
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-04 18:39:39 -0400

    view for chat created

---
+++ chatty/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-08-04.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
