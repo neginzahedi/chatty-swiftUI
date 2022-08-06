4d3dcecffb608ac3291a1ed0db1f452ee3edd4ce
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-06 18:35:46 -0400

    EditProfileView created

---
+++ chatty/EditProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileView.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-08-05.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text("edit profile")
+    }
+}
+
+struct EditProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileView()
+    }
+}
