10fc78cfb06847f599371899450728287dd7a11b
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-09 12:20:36 -0400

    ChangePasswordView created

---
+++ chatty/ChangepasswordView.swift
@@ -0,0 +1,20 @@
+//
+//  ChangepasswordView.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-08-09.
+//
+
+import SwiftUI
+
+struct ChangepasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChangepasswordView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangepasswordView()
+    }
+}
