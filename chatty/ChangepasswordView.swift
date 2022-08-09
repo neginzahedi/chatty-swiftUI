90853e676f94f254d5fb7b4fc6994bdbab4aad74
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-09 18:55:37 -0400

    UI done

--- chatty/ChangepasswordView.swift
+++ chatty/ChangepasswordView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ChangepasswordView: View {
+    
+    @State private var currentPassword: String = ""
+    @State private var newPassword: String = ""
+    @State private var newPasswordAgain: String = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            
+            Text("A secure password must be at least six characters and a combination of letters, numbers and special symbols.")
+                .foregroundColor(.gray)
+                .padding()
+            
+            Spacer()
+            
+            List{
+                
+                TextField("Enter Current Password", text: $currentPassword)
+                TextField("Enter New Password", text: $newPassword)
+                TextField("Enter New Password Again", text: $newPasswordAgain)
+                
+            }
+            
+        }
+        
+        .navigationBarTitle(Text("Change Password"), displayMode: .inline)
+        .navigationBarItems(trailing: Text("Save"))
+        
     }
+    
 }
 
 struct ChangepasswordView_Previews: PreviewProvider {
