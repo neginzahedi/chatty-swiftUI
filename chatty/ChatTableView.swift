ed9461bd512a9f8fdfc0bb7407fb2384b968a039
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-11 17:27:15 -0400

    navigationBarItems and navigationTitle added in nav bar instead of userProfile

--- chatty/ChatTableView.swift
+++ chatty/ChatTableView.swift
@@ -6,12 +6,8 @@
 //
 
 import SwiftUI
+import CoreMedia
 
-struct UserProfileView: View {
-    var body: some View {
-        Text("")
-    }
-}
 
 struct ChatTableView: View {
     
@@ -19,55 +15,19 @@ struct ChatTableView: View {
     
     var body: some View {
         NavigationView{
+            
             VStack{
-                // Nav bar: User Profile
-//                UserProfileView()
-                userProfile
-                // ScrollView: Sender Message List
                 sendersList
             }
-            .overlay(newMessageButton,alignment: .bottom)
-            .navigationBarHidden(true)
+            
+            .navigationBarBackButtonHidden(true)
+            .navigationTitle("Chats")
+            .navigationBarItems(trailing: Image(systemName: "square.and.pencil"))
         }
+        
+        
     }
     
-    // HStack: User Profile
-    private var userProfile: some View{
-        HStack(spacing: 16){
-            // user image
-            Image(systemName: "person.fill")
-                .font(.system(size: 34, weight: .heavy))
-                .padding(8)
-                .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black,lineWidth: 1))
-            
-            VStack(alignment: .leading, spacing: 4){
-                // user username
-                Text("username")
-                    .font(.system(size: 24,weight: .bold))
-                
-                HStack{
-                    Circle()
-                        .foregroundColor(.green)
-                        .frame(width: 14, height: 14)
-                    // user status
-                    Text("online")
-                        .font(.system(size: 12))
-                        .foregroundColor(Color(.lightGray))
-                }
-            }
-            
-            Spacer()
-            
-            NavigationLink(destination: SettingView()){
-                Image(systemName: "gear")
-                    .font(.system(size: 24,weight: .bold))
-                    .foregroundColor(Color(.label))
-                    
-            }
-            
-        }.padding()
-           
-    }
     
     // ScrollView: Senders List
     private var sendersList: some View{
@@ -85,50 +45,40 @@ struct ChatTableView: View {
     
     // HStack: Sender
     private var senderMessageRow: some View{
-        HStack(spacing: 16) {
-            Image(systemName: "person.fill")
-                .font(.system(size: 32))
-                .padding(8)
-                .overlay(RoundedRectangle(cornerRadius: 50).stroke(Color.black,lineWidth: 1))
-            
-            VStack(alignment: .leading){
-                Text("Username")
-                    .font(.system(size: 16,weight: .bold))
-                Text("Text messages")
-                    .font(.system(size: 14))
-                    .foregroundColor(Color(.lightGray))
-            }
-            
-            Spacer()
-            
-            Text("2m")
-                .font(.system(size: 14,weight: .semibold))
+        NavigationLink {
+            ChatView()
+        } label: {
+            HStack(spacing: 16) {
+                Image("kiss")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .padding()
+                
+                VStack(alignment: .leading){
+                    Text("Username")
+                        .font(.system(size: 16,weight: .bold))
+                    Text("Text messages")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color(.lightGray))
+                }
+                
+                Spacer()
+                
+                Text("2m")
+                    .font(.system(size: 14,weight: .semibold))
+            }.foregroundColor(.primary)
         }
+        
+        
     }
     
-    // Button: Create New Message
-    private var newMessageButton: some View{
-        Button{
-            
-        }label: {
-            HStack{
-                Spacer()
-                Text("+ New Message")
-                    .font(.system(size: 16, weight: .bold))
-                Spacer()
-            }
-            .foregroundColor(.white)
-            .padding(.vertical)
-            .background(Color.blue)
-            .cornerRadius(35)
-            .padding(.horizontal)
-            .shadow(radius: 15)
-        }
-    }
 }
 
 struct ChatTableView_Previews: PreviewProvider {
     static var previews: some View {
         ChatTableView()
+        
     }
 }
