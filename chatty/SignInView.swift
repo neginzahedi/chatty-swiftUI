2bc5c8561aa72167eeb297df2d4578ea746de42a
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-18 17:55:05 -0400

    navigate to MainView() when user successfully signed in

--- chatty/SignInView.swift
+++ chatty/SignInView.swift
@@ -5,13 +5,10 @@
 //  Created by Negin Zahedi on 2022-07-28.
 //
 // NOTES:
-// SignInView to sign in to existing account and display allerts if it goes wrong.
-
-//TODO: Display MainView() if successfully signed in
+// SignInView to sign in to existing account, display allerts if it goes wrong and navigate to MainView() if user successfully signed in.
 
 import SwiftUI
 import FirebaseAuth
-
 
 struct SignInView: View {
     
@@ -29,59 +26,47 @@ struct SignInView: View {
     
     // Boolean state that determines whether the screen should be visible
     @State private var isForgotPasswordScreen = false
-    //    @State private var isUserSignedIn = false
     @State private var isMainScreen = false
     
     var body: some View {
-        
-        // Vstack: all contents
         VStack(alignment: .center){
             Image("signin-view-img")
                 .resizable()
                 .scaledToFit()
                 .padding(.bottom,50)
-            
-            // VStack: SignIn Field
+            // SignIn Field
             VStack(alignment: .leading){
                 Text("Sign-In")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                
-                // Vstack: to enter Email
+                // Email
                 VStack(alignment: .leading){
-                    // label
                     Text("Email")
                         .font(.callout)
                         .bold()
-                    // input
                     TextField("Enter email ...", text: $email)
                         .disableAutocorrection(true)
                         .textFieldStyle(.roundedBorder)
                         .autocapitalization(.none)
                         .keyboardType(.emailAddress)
                 }.padding(5)
-                
-                // Vstack: to enter password
+                // password
                 VStack(alignment: .leading){
                     Text("Password")
                         .font(.callout)
                         .bold()
                     SecureField("Enter password ...", text: $password)
                         .textFieldStyle(.roundedBorder)
-                    
+                    // forgot password button to display ForgotPasswordView()
                     Button {
                         isForgotPasswordScreen = true
                     } label: {
-                        Text("Forget password?")
+                        Text("Forgot password?")
                             .foregroundColor(.gray)
-                        
                     }
                 }.padding(5)
-                
             }.padding()
-            
-            
-            // Button: create account
+            // sign in button
             Button("Sign in", action: signIn)
                 .frame(width: 200, height: 50, alignment: .center)
                 .background(.blue)
@@ -89,8 +74,7 @@ struct SignInView: View {
                 .foregroundColor(.white)
                 .font(.headline)
                 .padding()
-            
-            // sign up
+            // dismiss SignInView() to display WelcomeView()
             HStack(alignment:.center){
                 Text("Don't have an account?")
                 Button {
@@ -109,30 +93,26 @@ struct SignInView: View {
             }
         
         // fullScreenCover:
-        // when $isForgotPasswordScreen is true, ForgotPasswordView() shows up
+        // when $isForgotPasswordScreen is true, ForgotPasswordView() shows up.
             .fullScreenCover(isPresented: $isForgotPasswordScreen){
                 ForgotPasswordView()
             }
-        // when $isMainScreen is true, MainView() shows up
+        // when $isMainScreen is true, MainView() shows up.
             .fullScreenCover(isPresented: $isMainScreen){
                 MainView()
             }
-        
-        
-        
     }
     
-    // method to sign in user by using firebase signIn method or display alert if fails
+    // to sign in user with firebase signIn(), display alert if fails and then navigate user to MainView()
     private func signIn(){
         Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
-            
             if let e = error{
                 print("failed log in")
                 self.alertMessage = e.localizedDescription
                 self.isSignInFaildAlert = true
                 return
             }
-            //TODO: go to MainView()
+            // go to MaiView()
             print("succesfuly log in")
             isMainScreen = true
         }
