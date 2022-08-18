fc24816d66c4d45edbdffc428cee90a5d16c7219
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-18 17:58:00 -0400

    new struct for user properties added

---
+++ chatty/CurrentUser.swift
@@ -0,0 +1,14 @@
+//
+//  userModel.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-08-08.
+//
+
+import Foundation
+
+struct CurrentUser {
+    let uid: String
+    let emailAddress: String
+    let profileImageUrl:String
+}
