841f5263502e8cd8dd6579fe4502d640342d00e2
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-20 16:09:00 -0400

    added to Model

---
+++ chatty/Model/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-08-19.
+//
+
+import Foundation
+
+struct Contact{
+    let uid: String
+    let emailAddress: String
+    let profileImageUrl:String
+}
