051a2697671791cc4b7b6ea9e8a487d3cb31b9e5
Negin Zahedi <zahedi.negin@gmail.com>
2022-08-20 17:56:26 -0400

    moved to FirebaseManager

--- chatty/chattyApp.swift
+++ chatty/chattyApp.swift
@@ -6,15 +6,10 @@
 //
 
 import SwiftUI
-import Firebase
 
 @main
 struct chattyApp: App {
-    
-    init(){
-        FirebaseApp.configure()
-    }
-    
+
     var body: some Scene {
         WindowGroup {
             WelcomeView()
