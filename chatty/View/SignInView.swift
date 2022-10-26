de6b32331a273f5884b93ebff8ec2f8416192dd2
Negin Zahedi <zahedi.negin@gmail.com>
2022-10-26 18:06:58 -0400

    Sign in method moved to the view and view model is deleted. Full screen cover replaced by router class for navigation.

--- chatty/View/SignInView.swift
+++ chatty/View/SignInView.swift
@@ -4,121 +4,145 @@
 //
 //  Created by Negin Zahedi on 2022-07-28.
 //
-// SignInView: to sign-in existing account, display allerts if it goes wrong and navigate to MainView() if user successfully signed in.
-//
+// SignInView: Sign-in to created account
 
 import SwiftUI
 
 struct SignInView: View {
     
-    // To dismiss current view
-    @Environment(\.presentationMode) var presentationMode
-    
     // String State for user inputes
     @State private var email: String = ""
     @State private var password: String = ""
     
-    @State private var alertMessage: String = ""
+    // hide/show password
+    @State private var secured: Bool = true
+    
+    // To change view
+    @EnvironmentObject var viewRouter: ViewRouter
     
     // Boolean state that determines whether the alert should be visible
-    @State private var isSignInFaildAlert = false
+    @State var isSignInFaildAlert:Bool = false
     
-    // Boolean state that determines whether the screen should be visible
-    @State private var isForgotPasswordScreen = false
-    @State private var isMainScreen = false
+    // Alert Message
+    @State var alertMessage: String = ""
     
     var body: some View {
+        // Main VStack
         VStack(alignment: .center){
-            Image("signin-view-img")
+            // image
+            Image("sign-in")
                 .resizable()
                 .scaledToFit()
-                .padding(.bottom,50)
-            // SignIn Field
+                .padding()
+            Spacer()
+            // SignIn VStack
             VStack(alignment: .leading){
+                // Title
                 Text("Sign-In")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                // Email
+                // Email VStack
                 VStack(alignment: .leading){
+                    // Label
                     Text("Email")
                         .font(.callout)
                         .bold()
+                    // Input
                     TextField("Enter email ...", text: $email)
                         .disableAutocorrection(true)
                         .textFieldStyle(.roundedBorder)
                         .autocapitalization(.none)
                         .keyboardType(.emailAddress)
-                }.padding(5)
-                // Password
+                }.padding(5) // Email VStack
+                // Password VStack
                 VStack(alignment: .leading){
+                    // Label
                     Text("Password")
                         .font(.callout)
                         .bold()
-                    SecureField("Enter password ...", text: $password)
-                        .textFieldStyle(.roundedBorder)
-                    // Display ForgotPasswordView()
+                    // Input ZStack
+                    ZStack{
+                        HStack{
+                            if self.secured{
+                                SecureField("Enter password...", text: $password)
+                                    .disableAutocorrection(true)
+                            } else {
+                                TextField("Enter password...", text: $password)
+                                    .disableAutocorrection(true)
+                            }
+                            Button(action: {
+                                self.secured.toggle()
+                            }) {
+                                Image(systemName: self.secured ? "eye.slash": "eye")
+                                    .foregroundColor(.gray)
+                            }
+                        } // HStack
+                        .padding(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(red: 245.0/255, green: 245.0/255, blue: 245.0),lineWidth: 1))
+                    } // Input ZStack
+                    
+                    // Button ForgotPasswordView()
                     Button {
-                        isForgotPasswordScreen = true
+                        withAnimation {
+                            viewRouter.currentView = .pageForgotPasswordView
+                        }
                     } label: {
                         Text("Forgot password?")
                             .foregroundColor(.gray)
                     }
                 }.padding(5)
-            }.padding()
-            // Sign-in
-            Button {
-                signIn()
-            } label: {
-                Text("Sign in")
-                    .frame(width: 200, height: 50, alignment: .center)
-                    .background(.blue)
-                    .clipShape(Capsule())
-                    .foregroundColor(.white)
-                    .font(.headline)
-            }.padding()
-            // dismiss SignInView() to display WelcomeView()
-            HStack(alignment:.center){
-                Text("Don't have an account?")
+            }.padding() // SignIn VStack
+            
+            Spacer()
+            
+            // Buttons VStack
+            VStack {
+                // Button Sign-in
                 Button {
-                    presentationMode.wrappedValue.dismiss()
+                    FirebaseManager.shared.auth.signIn(withEmail: email, password: password){ authResult, error in
+                        if let e = error{
+                            print("Failed to Sign-in user: \(e)")
+                            self.alertMessage = e.localizedDescription
+                            self.isSignInFaildAlert = true
+                        } else {
+                            // go to MaiView()
+                            print("User succesfuly logged in.")
+                            withAnimation {
+                                viewRouter.currentView = .pageMainView
+                            }
+                        }
+                    }
                 } label: {
-                    Text("Sign-up")
-                        .foregroundColor(.gray)
-                }
-            }
-        }.padding()
+                    Text("Sign in")
+                        .font(.headline)
+                        .bold()
+                        .frame(width: 200, height: 50, alignment: .center)
+                        .background(.blue)
+                        .clipShape(Capsule())
+                        .foregroundColor(.white)
+                }.padding()
+                
+                // Display SignUpView()
+                HStack(alignment:.center){
+                    Text("Don't have an account?")
+                    Button {
+                        withAnimation {
+                            viewRouter.currentView = .pageSignUpView
+                        }
+                    } label: {
+                        Text("Sign-up")
+                            .foregroundColor(.gray)
+                    }
+                } // HStack
+            } // Buttons VStack
+        }.padding() // Main VStack
         
-        // Alerts:
-        // Display alert if faild to sign in
+        // alert: Display alert if faild to sign in
             .alert(alertMessage, isPresented: $isSignInFaildAlert) {
                 Button("Ok", role: .cancel) {}
             }
-        
-        // fullScreenCover:
-        // when $isForgotPasswordScreen is true, ForgotPasswordView() shows up.
-            .fullScreenCover(isPresented: $isForgotPasswordScreen){
-                ForgotPasswordView()
-            }
-        // when $isMainScreen is true, MainView() shows up.
-            .fullScreenCover(isPresented: $isMainScreen){
-                MainView()
-            }
     }
     
-    // to sign in user with firebase signIn(), display alert if fails and then navigate user to MainView()
-    private func signIn(){
-        FirebaseManager.shared.auth.signIn(withEmail: email, password: password){ authResult, error in
-            if let e = error{
-                print("failed log in")
-                self.alertMessage = e.localizedDescription
-                self.isSignInFaildAlert = true
-                return
-            }
-            // go to MaiView()
-            print("succesfuly log in")
-            isMainScreen = true
-        }
-    }
 }
 
 
