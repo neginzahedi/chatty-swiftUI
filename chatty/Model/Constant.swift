966429236a290e87e29afd50ca266b3ad89958e1
Negin Zahedi <zahedi.negin@gmail.com>
2022-11-06 01:06:56 -0400

    firebase db collection name

---
+++ chatty/Model/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-10-04.
+//
+
+import Foundation
+
+
+struct Constant {
+    
+    let collection_users = "users"
+    let collection_username_id = "username_uid"
+    let collection_messages = "messages"
+    let collection_messages_chat_users = "chat_users"
+
+    
+}
