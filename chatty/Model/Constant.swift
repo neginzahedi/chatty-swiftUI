19d7dd92c46e72e5e7901f9712b76bca021f262b
Negin Zahedi <zahedi.negin@gmail.com>
2022-11-06 17:25:40 -0500

    new attributes added

--- chatty/Model/Constant.swift
+++ chatty/Model/Constant.swift
@@ -10,10 +10,20 @@ import Foundation
 
 struct Constant {
     
+    // Collection "users"
     let collection_users = "users"
+    
+    // Collection "username_id"
     let collection_username_id = "username_uid"
+    
+    // Collection "messages"
     let collection_messages = "messages"
+    let collection_messages_fromUserID = "fromUserID"
+    let collection_messages_toUserID = "toUserID"
+    let collection_messages_text = "text"
+    
     let collection_messages_chat_users = "chat_users"
+    
 
     
 }
