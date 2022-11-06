43992313a61c180078644cd289a481b0f723bc25
Negin Zahedi <zahedi.negin@gmail.com>
2022-11-06 01:16:05 -0400

    ChatUser used in views related to chat with a contact

---
+++ chatty/Model/ChatTableUser.swift
@@ -0,0 +1,14 @@
+//
+//  ChatTableUser.swift
+//  chatty
+//
+//  Created by Negin Zahedi on 2022-10-31.
+//
+
+import Foundation
+
+struct ChatUser: Hashable {
+    let uid: String
+    let username: String
+    let profileImageURL:String
+}
