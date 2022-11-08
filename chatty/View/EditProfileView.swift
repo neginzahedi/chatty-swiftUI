c7786de9a42cc948cc3afe551ab820c79b8a8ff4
Negin Zahedi <zahedi.negin@gmail.com>
2022-11-07 20:33:32 -0500

    when user delete the account the current view changed to welcomeView()

--- chatty/View/EditProfileView.swift
+++ chatty/View/EditProfileView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Negin Zahedi on 2022-08-05.
 //
-// EditProfileView contains user's saved name, email address, status and Password which can be changed as well.
+// EditProfileView: cuurent user can check saved info, change them and delete account.
 //
 
 import SwiftUI
@@ -12,15 +12,22 @@ import SDWebImageSwiftUI
 
 struct EditProfileView: View {
     
+    // view model to fetch current user info
     @ObservedObject var vm = MainViewModel()
     
-    @State private var name: String = ""
+    @State private var username: String = ""
     @State private var email: String = ""
     @State private var status: String = ""
     
     @State var shouldShowImagePicker = false
+    @State var isDeleteAccountClicked = false
     
     @State var img: UIImage?
+    
+    // To change current view
+    @EnvironmentObject var viewRouter: ViewRouter
+    
+    let const = Constant()
     
     var body: some View {
         VStack{
@@ -37,29 +44,43 @@ struct EditProfileView: View {
                         //.clipped() - square
                         
                     }else{
-                        WebImage(url: URL(string: vm.currentUser?.profileImageUrl ?? "profileImageURL" ))
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 300, height: 300)
-                            .cornerRadius(150)
+                        if vm.currentUser?.profileImageUrl == ""{
+                            Image("profile")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 300, height: 300)
+                                .cornerRadius(150)
+                        } else {
+                            WebImage(url: URL(string: vm.currentUser?.profileImageUrl ?? "profile"))
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 300, height: 300)
+                                .cornerRadius(150)
+                        }
                     }
                 }
-            }
+            }.padding()
+            
             List{
                 // TODO: change user info
                 Section{
-                    HStack{
-                        Text("Username")
-                        Spacer()
-                        Spacer()
-                        TextField(vm.currentUser?.username ?? "username", text: $name)
+                    NavigationLink {
+                        ChangeUsernameView(vm: self.vm)
+                    } label: {
+                        Text(vm.currentUser?.username ?? "username")
                     }
-                    HStack{
-                        Text("Status")
-                        Spacer()
-                        Spacer()
-                        TextField(vm.currentUser?.status ?? "status", text: $status)
+                } header: {
+                    Text("username")
+                }
+                
+                Section{
+                    NavigationLink {
+                        ChangeStatusView(vm: self.vm)
+                    } label: {
+                        Text(vm.currentUser?.status ?? "status")
                     }
+                } header: {
+                    Text("Status")
                 }
                 
                 // Password Section: navigates to ChangepasswordView
@@ -67,18 +88,19 @@ struct EditProfileView: View {
                     NavigationLink {
                         ChangepasswordView()
                     } label: {
-                        Text("Password")
+                        Text("Change Password")
                     }
-                    NavigationLink {
-                        EmptyView()
+                    // delete account
+                    Button {
+                        isDeleteAccountClicked.toggle()
                     } label: {
                         Text("Delete Account")
+                            .foregroundColor(.red)
                     }
-                    
-                }
+                } // password section
             }.listStyle(InsetGroupedListStyle())
                 .environment(\.horizontalSizeClass, .regular)
-                .navigationTitle("Edit Profile")
+                .navigationTitle("Edit Profile").navigationBarTitleDisplayMode(.inline)
                 .toolbar{
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button {
@@ -89,11 +111,56 @@ struct EditProfileView: View {
                     }
                 }
         }
+        // actionSheet: confirm delete account
+        .actionSheet(isPresented: $isDeleteAccountClicked) {
+            .init(title: Text("Delete Account"), message: Text("Are you sure you want to delete your account permenantly?"), buttons: [
+                .destructive(Text("Delete"), action: {
+                    deleteUser()
+                }),
+                .cancel()
+            ])
+        }
+        // fullScreenCover
         .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
             ImagePicker(image: $img )
         }
-        .onAppear(){
-            self.vm.fetchCurrentUser()
+        //        .onAppear(){
+        //            self.vm.fetchCurrentUser()
+        //        }
+    }
+    
+    // TODO: ask to sign in again
+    // TODO: remove user from Firestore DB
+    func deleteUser() {
+        let user = FirebaseManager.shared.auth.currentUser
+        let userID = FirebaseManager.shared.auth.currentUser?.uid ?? ""
+        let username = vm.currentUser?.username ?? ""
+        user?.delete { error in
+            if let error = error {
+                print(error)
+            } else {
+                print("Account deleted.")
+                // users DB
+                FirebaseManager.shared.firestoreDB.collection(const.collection_users).document(userID).delete() { err in
+                    if let err = err {
+                        print("Error removing document: \(err)")
+                    } else {
+                        print("Document successfully removed!")
+                    }
+                }
+                
+                // username_id DB
+                FirebaseManager.shared.firestoreDB.collection(const.collection_usernames).document(username).delete() { err in
+                    if let err = err {
+                        print("Error removing document: \(err)")
+                    } else {
+                        print("Document successfully removed!")
+                        withAnimation {
+                            self.viewRouter.currentView = .pageWelcomeView
+                        }
+                    }
+                }
+            }
         }
     }
     
@@ -126,7 +193,7 @@ struct EditProfileView: View {
             return
         }
         
-        FirebaseManager.shared.firestoreDB.collection("registeredUsers").document(userID).updateData([
+        FirebaseManager.shared.firestoreDB.collection(const.collection_users).document(userID).updateData([
             "profileImageURL" : userProfileImageURL.absoluteString
         ]) { error in
             if let e = error {
@@ -135,6 +202,29 @@ struct EditProfileView: View {
             }
             else{
                 print("image saved to db")
+            }
+        }
+    }
+    
+    func saveUsername(){
+        guard let userID = FirebaseManager.shared.auth.currentUser?.uid else {
+            print("user not found")
+            return
+        }
+        
+        if username.isEmpty {
+            print("no new username")
+        } else {
+            FirebaseManager.shared.firestoreDB.collection(const.collection_users).document(userID).updateData([
+                "username" : self.username
+            ]) { error in
+                if let e = error {
+                    print(e)
+                    return
+                }
+                else{
+                    print("username updated")
+                }
             }
         }
     }
