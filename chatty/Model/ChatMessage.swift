6019f68e6628637d65e043fe547d06fb74cad50b
Negin Zahedi <zahedi.negin@gmail.com>
2022-11-14 18:54:43 -0500

    change ChatMessage struct to Codable

--- chatty/Model/ChatMessage.swift
+++ chatty/Model/ChatMessage.swift
@@ -6,22 +6,13 @@
 //
 
 import Foundation
+import FirebaseFirestoreSwift
 
-
-struct ChatMessage: Identifiable{
+struct ChatMessage: Codable, Identifiable{
     
-    let const = Constant()
-
-    let documentID: String
-    var id: String {documentID}
-    let fromID: String
-    let toID: String
+    //let documentID: String
+    @DocumentID var id: String?
+    let fromUserID: String
+    let toUserID: String
     let text: String
-    
-    init(documentID: String, data: [String: Any]){
-        self.documentID = documentID
-        self.fromID = data[const.collection_messages_fromUserID] as? String ?? ""
-        self.toID = data [const.collection_messages_toUserID] as? String ?? ""
-        self.text = data[const.collection_messages_text] as? String ?? ""
-    }
 }
